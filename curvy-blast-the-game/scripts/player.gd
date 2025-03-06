5af79f3e0c5c7a9ef40a6a61b453fe62b82e7aee
Ethan Guillot <ethan.guillot@universite-paris-saclay.fr>
2025-03-06 16:17:17 +0100

    ajout de la prise en compte des annimations (pour l'instant sans annimation pour cause de pas d'assets)

--- curvy-blast-the-game/scripts/player.gd
+++ curvy-blast-the-game/scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -11,8 +13,8 @@ var XYScale = 30
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var expression = Expression.new()
-var cannoned = true
-var inCannon = false 
+var canoned = true
+var inCanon = false 
 var functionCurve  = "2*cos(x)+4"
 
 func calcule(f):
@@ -26,50 +28,59 @@ func calcule(f):
 		return result
 
 func shoot(f):
-	if(inCannon):
+	if(inCanon):
 		var error = expression.parse(f)
 		if error == OK:
-			cannoned = true
-			inCannon = false
+			canoned = true
+			inCanon = false
 			
 			functionCurve = f
 			
 			#changer d'annimation
 			
 func enterCanon(x,y):
-	inCannon = true
-	cannoned = false
+	inCanon = true
+	canoned = false
 	
 	#ajouter l'apparition de l'interface
 	#changer d'annimation
 
+func applyCanonMov(delta):
+	position.x += CANNON_VELOCITY
+	var res = calcule(functionCurve.replace("x","("+str(position.x)+"/"+str(XYScale)+".0)"))
+	if (res != null):
+		position.y = -res*XYScale
+	var collision = move_and_collide(velocity * delta,true)
+	if (collision):
+		canoned = false
+
 func _physics_process(delta):
-	
-	if(cannoned):
-		position.x += CANNON_VELOCITY
+	if(inCanon):
+		animation.play("inCanon")
 		
-		var res = calcule(functionCurve.replace("x","("+str(position.x)+"/"+str(XYScale)+".0)"))
+	elif(canoned):
+		applyCanonMov(delta)
+		animation.play("enVol")
 		
-		if (res != null):
-			position.y = -res*XYScale
-			
-		var collision = move_and_collide(velocity * delta,true)
-		if (collision):
-			cannoned = false
-			
 	else:
+		animation.play("idle")
+		
 		# Add the gravity.
 		if not is_on_floor():
 			velocity.y += gravity * delta
+			animation.play("fall")
 		# Handle jump.
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
+			animation.play("jump")
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.get_axis("ui_left", "ui_right")
 		if direction:
 			velocity.x = direction * SPEED
+			animation.play("jump")
+			if(direction < 0):animation.flip_v
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 
