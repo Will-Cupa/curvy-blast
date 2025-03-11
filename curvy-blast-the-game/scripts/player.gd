c762476c0a410474dedc0309942d0a4ee624a09d
Sandoz Alain <alain.sandoz@universite-paris-saclay.fr>
2025-03-11 18:00:10 +0100

    désolé les amis

---
+++ curvy-blast-the-game/scripts/player.gd
@@ -0,0 +1,120 @@
+class_name Player extends CharacterBody2D
+
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+const CANNON_VELOCITY = 3
+var XYScale = 30
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var expression = Expression.new()
+var canoned = false
+var inCanon = false 
+var functionCurve  = "2*cos(x)+4"
+var xRelatif = 0
+var yRelatif = 0
+var yAvantCanon = 0
+
+func calcule(f):
+	var error = expression.parse(f.replace("x","("+str(xRelatif)+"/"+str(XYScale)+".0)"))
+	if error != OK:
+		#print(expression.get_error_text())
+		return
+	var result = expression.execute()
+	if not expression.has_execute_failed():
+		#print(result)
+		return result
+
+func shoot(f):
+	if(inCanon):
+		var error = expression.parse(f)
+		if error == OK:
+			canoned = true
+			inCanon = false
+			
+			functionCurve = f
+			
+			#changer d'annimation
+			
+func enterCanon(x,y):
+	inCanon = true
+	canoned = false
+	position.x = x
+	position.y = y
+	#ajouter l'apparition de l'interface
+	xRelatif = 0
+	yRelatif = 0
+	yAvantCanon = position.y
+
+func applyCanonMov(delta):
+	
+	position.x += CANNON_VELOCITY
+	xRelatif += CANNON_VELOCITY
+	var res = calcule(functionCurve)
+	if (res != null):
+		yRelatif = -res*XYScale
+		position.y = yAvantCanon + yRelatif
+	var collision = move_and_collide(velocity * delta,true)
+	if (collision):
+		canoned = false
+
+func animationGestion():
+	if(inCanon):
+		animation.play("inCanon")
+		
+	elif(canoned):
+		animation.play("enVol")
+		
+	else:
+		if is_on_floor():
+			if Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+				animation.play("walk")
+			elif Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+				animation.play_backwards("walk")
+			else:
+				animation.play("idle")
+			
+		else:
+			if velocity.y < 0:
+				animation.play("jump")
+			else:
+				animation.play("fall")
+			
+		
+		
+		
+
+func _physics_process(delta):
+	
+	animationGestion()
+	
+	if(inCanon):
+		pass
+		
+	elif(canoned):
+		applyCanonMov(delta)
+		
+	else:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+			
+			if(direction < 0):animation.flip_v
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		move_and_slide()
