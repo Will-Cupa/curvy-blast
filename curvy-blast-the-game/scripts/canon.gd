f4b97b82abfbae2925bed5a6c30da491eb3debad
Alain Sandoz <alain.sandoz@universite-paris-saclay.fr>
2025-03-13 11:13:55 +0100

    fonction opti

--- curvy-blast-the-game/scripts/canon.gd
+++ curvy-blast-the-game/scripts/canon.gd
@@ -5,7 +5,6 @@ var player
 var playerInCanon = false
 var expression = Expression.new()
 var expressionReady = false
-
 @onready var sprite = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
@@ -55,28 +54,35 @@ func valueAt(x : float) -> float:
 	return 0.0
 
 
-func getOptiPoint(x1,x2, space, treshold) -> float:
+func getOptiPoint(x1, space, treshold) -> float:
+	print(space)
+	var x2 = x1 + space
 	var x3 = x1 + space/2
 	var y1 = valueAt(x1)
 	var y2 = valueAt(x2)
-	var y3 = min(y1, y2) + abs(y1 - y2)
+	var y3 = min(y1, y2) + abs(y1 - y2)/2
 	var yTarget = valueAt(x3)
 	
-	if(abs(y3 - yTarget) > treshold):
-		
+	if abs(y3 - yTarget) > treshold && space > 1:
+		return getOptiPoint(x1, space/2, treshold)
 	return x2
 
 func _draw() -> void:
-	var space = 20
+	var space = 50
 	var maxWidth = get_viewport_rect().size.x - position.x
 	var p1
 	var p2
 	var i = 0
+	var i2
+	var nbPoints = 1
 	if expressionReady:
 		while i <= maxWidth:
 			p1 = Vector2(i, valueAt(i)) 
-			p2 = Vector2(i+space, valueAt(i+space))
-			i += space
+			i2 = getOptiPoint(i, space, 0.1)
+			p2 = Vector2(i2, valueAt(i2))
+			i = i2
+			nbPoints += 1
 			draw_line(p1,p2,Color.RED,1)
-			sprite.rotation = slopeAt(0,0.1)
-			print(slopeAt(0,0.1))
+			#sprite.rotation = slopeAt(0,0.1)
+			#print(slopeAt(0,0.1))
+		print(nbPoints)
