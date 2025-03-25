79501bbdde61b048942565387e9acf8b22107ebd
JasonZOH <jasonzoh2@gmail.com>
2025-03-25 16:25:09 +0100

    'Textbox' script achevé

--- curvy-blast-the-game/scripts/text_box.gd
+++ curvy-blast-the-game/scripts/text_box.gd
@@ -21,3 +21,30 @@ func display_text(text_to_display: String):
 	custom_minimum_size.x = min(size.x, MAX_WIDTH)
 	if size.x > MAX_WIDTH:
 		label.autowrap_mode = TextServer.AUTOWRAP_WORD
+		await resized #waiting for x resize
+		await resized #waiting for y resize
+		custom_minimum_size.y = size.y
+	
+	global_position.x -= size.x / 2
+	global_position.y -= size.y + 15
+	
+	label.text = ""
+	
+func _display_text():
+	label.text = text[letter_index]
+	letter_index += 1
+	if letter_index >= text.length():
+		finished_displaying.emit()
+		return
+	
+	match text[letter_index]:
+		"!", ".", ",", ";", "?":
+			timer.start(punctuation_time)
+		" ":
+			timer.start(space_time)
+		_:
+			timer.start(letter_time)
+
+
+func _on_letter_display_timer_timeout():
+	_display_text()
