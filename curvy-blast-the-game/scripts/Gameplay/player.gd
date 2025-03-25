9a6c3824d19402598368d9cd9761874c608bb8b4
Alain Sandoz <alain.sandoz@universite-paris-saclay.fr>
2025-03-25 17:44:21 +0100

    heu

--- curvy-blast-the-game/scripts/Gameplay/player.gd
+++ curvy-blast-the-game/scripts/Gameplay/player.gd
@@ -7,7 +7,7 @@ signal death
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-const CANNON_VELOCITY = 3
+const CANON_VELOCITY = 3
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -45,8 +45,8 @@ func enterCanon(x,y):
 
 func applyCanonMov(delta):
 	
-	position.x += CANNON_VELOCITY
-	xRelatif += CANNON_VELOCITY
+	position.x += CANON_VELOCITY
+	xRelatif += CANON_VELOCITY
 	var res = calculeCurve()
 	if (res != null):
 		yRelatif = res
@@ -61,7 +61,7 @@ func animationGestion():
 		
 	elif(canoned):
 		animation.play("enVol")
-		animation.rotation = atan(functionCurve.slopeAt(position.x, 0.1))
+		animation.rotation = atan(-1/functionCurve.slopeAt(position.x, 0.01))
 		
 	else:
 		animation.rotation = 0
