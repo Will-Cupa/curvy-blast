8b6f7a39c8b8a32a981c6c725e11707f5fed049e
JasonZOH <jasonzoh2@gmail.com>
2025-03-25 12:59:44 +0100

    Debut de realisation des boites de dialogue pour le tutoriel

---
+++ curvy-blast-the-game/scripts/text_box.gd
@@ -0,0 +1,23 @@
+extends MarginContainer
+
+@onready var label = $MarginContainer/Label
+@onready var timer = $LetterDisplayTimer
+
+const MAX_WIDTH = 256
+var text = ""
+var letter_index = 0
+
+var letter_time = 0.03
+var space_time = 0.06
+var punctuation_time = 0.2
+
+signal finished_displaying()
+
+func display_text(text_to_display: String):
+	text = text_to_display
+	label.text = text_to_display
+	
+	await resized
+	custom_minimum_size.x = min(size.x, MAX_WIDTH)
+	if size.x > MAX_WIDTH:
+		label.autowrap_mode = TextServer.AUTOWRAP_WORD
