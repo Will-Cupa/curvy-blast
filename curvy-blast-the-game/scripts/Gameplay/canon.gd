5609a9d4dfd9c163707b73a547a6b60f5dd334e2
Will-Cupa <adjm.sandoz@outlook.fr>
2025-04-06 12:15:07 +0200

    clean canon

--- curvy-blast-the-game/scripts/Gameplay/canon.gd
+++ curvy-blast-the-game/scripts/Gameplay/canon.gd
@@ -48,6 +48,7 @@ func _on_area_2d_body_entered(body : Object) -> void:
 		player = body #On garde la reference
 		player.enterCanon(position.x,position.y)
 		playerInCanon = true #Le joueur est dans le canon
+		queue_redraw()
 
 
 func getOptiPoint(x1, space, treshold) -> float:
@@ -70,27 +71,28 @@ func _draw() -> void:
 	var p2
 	var i = 0
 	var i2
-	if expressionReady && playerInCanon:
-		while i <= maxWidth:
-			p1 = Vector2(i, expression.valueAt(i)) 
-			i2 = getOptiPoint(i, space, 0.1)
-			p2 = Vector2(i2, expression.valueAt(i2))
-			i = i2
-			nbPoints += 1
-			draw_line(p1,p2,Color.RED,1)
-			#sprite.texture.get_width()
-			sprite.rotation = atan(expression.slopeAt(0,0.1))
-			sprite.position.y = expression.valueAt(0)
-		print(nbPoints)
-	
-	if playerInCanon:	
-		var default_font : Font = ThemeDB.fallback_font;	
+	if playerInCanon:
+		drawScale()
 		
-		for n in range(40):
-			draw_string(default_font, Vector2(n*expression.getXYScale(),0), str(n), HORIZONTAL_ALIGNMENT_LEFT, 32, 16, Color.BLACK)
-			draw_string(default_font, Vector2(0,-n*expression.getXYScale()), str(n), HORIZONTAL_ALIGNMENT_LEFT, 32, 16, Color.BLACK)
-				
-				
+		if expressionReady:
+			while i <= maxWidth:
+				p1 = Vector2(i, expression.valueAt(i)) 
+				i2 = getOptiPoint(i, space, 0.1)
+				p2 = Vector2(i2, expression.valueAt(i2))
+				i = i2
+				draw_line(p1,p2,Color.WHITE,2)
+				sprite.rotation = atan(expression.slopeAt(0,0.1))
+				sprite.position.y = expression.valueAt(0)
+		
+		
+
+func drawScale():
+	var default_font : Font = ThemeDB.fallback_font;	
+	
+	for n in range(40):
+		draw_string(default_font, Vector2(n*expression.getXYScale(),0), str(n), HORIZONTAL_ALIGNMENT_LEFT, 32, 16, Color.WHITE)
+		draw_string(default_font, Vector2(0,-n*expression.getXYScale()), str(n), HORIZONTAL_ALIGNMENT_LEFT, 32, 16, Color.WHITE)
+
 func setScale(scale):
 	expression.XYScale = scale
 				
