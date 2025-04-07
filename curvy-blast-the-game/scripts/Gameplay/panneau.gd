8f7ea97ee7cf4afdfc0076d02f0f6fd9c943a13a
Ethan Guillot <ethan.guillot@universite-paris-saclay.fr>
2025-04-07 15:58:23 +0200

    modife dialogue

--- curvy-blast-the-game/scripts/Gameplay/panneau.gd
+++ curvy-blast-the-game/scripts/Gameplay/panneau.gd
@@ -5,4 +5,4 @@ extends Node2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		var lines = (panneau.get_meta("lignes"))
-		DialogManager.start_dialog(global_position, lines)
+		DialogManager.start_dialog(global_position, lines, self)
