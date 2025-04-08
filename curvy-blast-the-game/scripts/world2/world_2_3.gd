af46102e7b4ce21246b9197b51c7187cf75cda0d
JasonZOH <jasonzoh2@gmail.com>
2025-04-08 18:19:24 +0200

    Probleme de collectable corrigé

--- curvy-blast-the-game/scripts/world2/world_2_3.gd
+++ curvy-blast-the-game/scripts/world2/world_2_3.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var canon: StaticBody2D = $TileMap/canon
+var total_colletable = 0
+const MAX_COLLECTABLE = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,3 +12,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+signal colectable_collected(value : int)
+
+func collect_collectable(value: int):
+	total_colletable += value
+	total_colletable = min(MAX_COLLECTABLE, total_colletable)
+	print(total_colletable)
+	self.emit_signal("colectable_collected", total_colletable)
