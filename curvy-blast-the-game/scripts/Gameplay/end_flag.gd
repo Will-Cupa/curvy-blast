829c114d5884731ded67e9faca8a04f2c47f01c5
Ethan Guillot <ethan.guillot@universite-paris-saclay.fr>
2025-04-08 18:32:40 +0200

    patch dialogue 2

--- curvy-blast-the-game/scripts/Gameplay/end_flag.gd
+++ curvy-blast-the-game/scripts/Gameplay/end_flag.gd
@@ -22,4 +22,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_win_timer_timeout() -> void:
 	var total_collectable = get_parent().total_colletable
 	Global.collectables_by_level[Global.current_level] = total_collectable
+	DialogManager.clean()
 	get_tree().change_scene_to_file("res://scenes/GameLevel/LevelManager/world_select.tscn")
